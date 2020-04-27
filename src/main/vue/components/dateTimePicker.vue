407a099e644074b4d53a23b19900a5aa735d8ffd
nhille <nhille@techfak.uni-bielefeld.de>
2020-04-27 12:05:12 +0200

    refs #14109 @2.0h improved the timer component

--- src/main/vue/components/dateTimePicker.vue
+++ src/main/vue/components/dateTimePicker.vue
@@ -3,8 +3,8 @@
         <v-row>
             <v-col cols="12" lg="6">
                 <v-menu
-                    ref="menu1"
-                    v-model="menu1"
+                    ref="menu"
+                    v-model="menu"
                     :close-on-content-click="false"
                     transition="scale-transition"
                     offset-y
@@ -21,10 +21,12 @@
                         ></v-text-field>
                     </template>
                     <v-date-picker
+                        id="datePicker"
                         v-model="date"
                         no-title
-                        @input="menu1 = false"
+                        @input="closeMenuAndUpdate"
                         show-current="true"
+                        locale="de"
                         :min="new Date().toISOString().substr(0, 10)"
                         max="2100-01-01"
                     ></v-date-picker>
@@ -41,8 +43,7 @@ export default {
     data: (vm) => ({
         date: new Date().toISOString().substr(0, 10),
         dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
-        menu1: false,
-        menu2: false,
+        menu: false,
     }),
     computed: {
         time() {
@@ -51,24 +52,27 @@ export default {
         },
     },
 
-    watch: {
-        date(val) {
-            this.dateFormatted = this.formatDate(this.date)
-        },
-    },
-
     methods: {
         formatDate(date) {
             if (!date) return null
 
             const [year, month, day] = date.split('-')
-            return `${month}/${day}/${year}`
+            return `${day}.${month}.${year}`
         },
         parseDate(date) {
             if (!date) return null
 
-            const [month, day, year] = date.split('/')
+            const [day, month, year] = date.split('.')
             return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+        },
+        reformatDate() {
+            this.dateFormatted = this.formatDate(this.date)
+            console.log('Hallo')
+        },
+
+        closeMenuAndUpdate() {
+            this.menu = false
+            this.reformatDate()
         },
     },
 }
--- src/main/vue/pages/QuestionCreation.vue
+++ src/main/vue/pages/QuestionCreation.vue
@@ -8,6 +8,9 @@
                             <h3>Umfrage erstellen</h3>
                         </v-col>
                     </v-row>
+                    <div>
+                        console.log("Hallo")
+                    </div>
                     <v-row>
                         <v-col cols="12" md="4">
                             <v-text-field
