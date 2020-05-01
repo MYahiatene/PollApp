4d7745dbb8cae55c01211e35f733e20ea3ed68bc
abebenroth <abebenroth@techfak.uni-bielefeld.de>
2020-05-01 12:45:51 +0200

    build the rough layout of the participant page, closes #14245 @1.0h

--- src/main/vue/pages/Participant.vue
+++ src/main/vue/pages/Participant.vue
@@ -1,13 +1,57 @@
 <template>
-
+    <v-container>
+        <v-subheader class="display-1"> Umfrage 1</v-subheader>
+        <v-content>
+            <v-row>
+                <v-col cols="8">
+                    <v-list v-for="question in questions" :key="question.id" two-line>
+                        <v-card class="mx-auto">
+                            <v-card-title> {{ question.text }}</v-card-title>
+                            <div v-if="question.type === 'textQuestion'">
+                                <v-text-field label="Antwort"> </v-text-field>
+                            </div>
+                            <div v-else-if="question.type === 'choiceQuestion'">
+                                <v-list v-for="answer in question.answers" :key="answer.text">
+                                    <v-checkbox class="ma-4" :label="answer.text"></v-checkbox>
+                                </v-list>
+                            </div>
+                        </v-card>
+                    </v-list>
+                </v-col>
+            </v-row>
+        </v-content>
+    </v-container>
 </template>
 
 <script>
-    export default {
-        name: "Participant"
-    }
+export default {
+    name: 'Participant',
+    data: () => ({
+        questions: [
+            {
+                id: '1',
+                text: 'Was für eine Frage?',
+                type: 'textQuestion',
+            },
+            {
+                id: '2',
+                text: 'Was für zwei Fragen?',
+                type: 'choiceQuestion',
+                answers: [
+                    {
+                        text: 'a',
+                    },
+                    {
+                        text: 'b',
+                    },
+                    {
+                        text: 'c',
+                    },
+                ],
+            },
+        ],
+    }),
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
