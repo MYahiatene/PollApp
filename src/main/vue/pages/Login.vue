a609b9e7197edb15fa67ff6cea869a38ef5c3cc8
Luca Schlegel <admin@lucaschlegel.de>
2020-05-12 14:03:24 +0200

    Login resettet, Beispiel kopiert

--- src/main/vue/pages/Login.vue
+++ src/main/vue/pages/Login.vue
@@ -1,28 +1,6 @@
-<template>
-    <b-container>
-        <b-row>
-            <b-col cols="6" offset="3" class="mt-3">
-                <b-card header="Login">
-                    <b-form @submit.prevent="login">
-                        <b-form-group label="Username:">
-                            <b-form-input v-model="auth.username" type="text"></b-form-input>
-                        </b-form-group>
-                        <b-form-group label="Passwort:">
-                            <b-form-input v-model="auth.password" type="password"></b-form-input>
-                        </b-form-group>
-                        <b-button type="submit" variant="primary">Login</b-button>
-                    </b-form>
-                    <b-alert variant="danger" :show="authenticated === false">
-                        Bitte überprüfen Sie Ihre Angaben.
-                    </b-alert>
-                </b-card>
-            </b-col>
-        </b-row>
-    </b-container>
-</template>
-
 <script>
-import { mapGetters, mapMutations } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
+
 export default {
     name: 'Login',
     data() {
@@ -39,15 +17,10 @@ export default {
         }),
     },
     methods: {
-        ...mapMutations(['authenticate']),
+        ...mapActions(['requestToken']),
         login() {
-            this.authenticate(this.auth)
-            if (this.authenticated) {
-                this.$router.push('/')
-            }
+            this.requestToken(this.auth).then(() => this.$router.push('/'))
         },
     },
 }
 </script>
-
-<style scoped></style>
