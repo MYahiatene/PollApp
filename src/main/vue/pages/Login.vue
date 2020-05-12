2da98510f2bc6c62ed01133d74c9834f86376db8
Luca Schlegel <admin@lucaschlegel.de>
2020-05-12 14:04:25 +0200

    Login resettet

--- src/main/vue/pages/Login.vue
+++ src/main/vue/pages/Login.vue
@@ -1,3 +1,26 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col cols="6" offset="3" class="mt-3">
+                <b-card header="Login">
+                    <b-form @submit.prevent="login">
+                        <b-form-group label="Username:">
+                            <b-form-input v-model="auth.username" type="text"></b-form-input>
+                        </b-form-group>
+                        <b-form-group label="Passwort:">
+                            <b-form-input v-model="auth.password" type="password"></b-form-input>
+                        </b-form-group>
+                        <b-button type="submit" variant="primary">Login</b-button>
+                    </b-form>
+                    <b-alert variant="danger" :show="authenticated === false">
+                        Bitte überprüfen Sie Ihre Angaben.
+                    </b-alert>
+                </b-card>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
 <script>
 import { mapActions, mapGetters } from 'vuex'
 
@@ -24,3 +47,5 @@ export default {
     },
 }
 </script>
+
+<style scoped></style>
