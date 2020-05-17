3d5dd2078647de43e56bb7874dbeef9acf424c8d
abebenroth <abebenroth@techfak.uni-bielefeld.de>
2020-05-17 12:50:27 +0200

    fixed the bean exception

--- src/main/java/gpse/umfrato/domain/security/SecurityConfig.java
+++ src/main/java/gpse/umfrato/domain/security/SecurityConfig.java
@@ -23,7 +23,7 @@ import javax.annotation.Resource;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Resource(name = "User.UserService")
+    @Resource // (name = "userService")
     private UserDetailsService userDetailsService;
 
     @Autowired
