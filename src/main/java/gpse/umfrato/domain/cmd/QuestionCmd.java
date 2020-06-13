7a746ff9ad4fc58973626be7709358c53e7811ae
Luca Schlegel <admin@lucaschlegel.de>
2020-06-13 17:02:51 +0200

    QuestionCMD editiert um die benötigten Attribute zu enthalten sowie Funktion getQuestion refs #15351

--- src/main/java/gpse/umfrato/domain/cmd/QuestionCmd.java
+++ src/main/java/gpse/umfrato/domain/cmd/QuestionCmd.java
@@ -1,7 +1,10 @@
 package gpse.umfrato.domain.cmd;
 
+import gpse.umfrato.domain.poll.Poll;
+import gpse.umfrato.domain.question.Question;
 import lombok.Data;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Data
@@ -9,12 +12,64 @@ public class QuestionCmd {
 
     private String pollId;
 
-    private String questionId;
+    private float endValue;
+
+    private float startValue;
+
+    private float stepSize;
+
+    private String belowMessage;
+
+    private String aboveMessage;
+
+    private boolean hideValues;
+
+    private boolean textMultiline;
+
+    private int textMinimum;
+
+    private int textMaximum;
+
+    private Long questionId;
+
+    private Long categoryId;
 
     private String questionMessage;
 
     private List<String> answerPossibilities;
 
     private String questionType;
+
+    private boolean userAnswers;
+
+    private int numberOfPossibleAnswers;
+
+    public Question getQuestion() {
+        final Question question = new Question(questionMessage, answerPossibilities, questionType);
+        switch(question.getQuestionType()){
+            case "TextQuestion":
+                question.setTextMaximum(textMaximum);
+                question.setTextMinimum(textMinimum);
+                question.setTextMultiline(true);
+                break;
+            case "RangeQuestion":
+            case "SliderQuestion":
+                question.setEndValue(endValue);
+                question.setStartValue(startValue);
+                question.setStepSize(stepSize);
+                question.setBelowMessage(belowMessage == "" ? "" : belowMessage);
+                question.setAboveMessage(aboveMessage == "" ? "" : aboveMessage);
+                break;
+            case "ChoiceQuestion":
+                question.setAnswerPossibilities(answerPossibilities);
+                question.setNumberOfPossibleAnswers(numberOfPossibleAnswers);
+                break;
+            case "SortQuestion":
+                question.setAnswerPossibilities(answerPossibilities);
+                break;
+            default: throw new NullPointerException("Don't do that");
+        }
+        return question;
+    }
 
 }
