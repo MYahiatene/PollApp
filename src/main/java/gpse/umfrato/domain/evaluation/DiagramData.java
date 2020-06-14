4fe2c272aecb3e3378dfd79deaa06685f34617ef
Jan Niemeier <jniemeier@techfak.uni-bielefeld.de>
2020-06-14 02:05:37 +0200

    auswertung für rangequestions repariert

--- src/main/java/gpse/umfrato/domain/evaluation/DiagramData.java
+++ src/main/java/gpse/umfrato/domain/evaluation/DiagramData.java
@@ -36,7 +36,7 @@ public class DiagramData {
 
     @Getter
     @Setter
-    private class ChoiceData implements QuestionData {
+    protected static class ChoiceData implements QuestionData {
         /* default */ long questionId;
         /* default */ String questionTitle;
         private List<String> answerPossibilities;
@@ -131,7 +131,7 @@ public class DiagramData {
 
     @Getter
     @Setter
-    private class TextData implements QuestionData {
+    protected static class TextData implements QuestionData {
         /* default */ long questionId;
         /* default */ String questionTitle;
         private List<Long> ids = new ArrayList<>();
@@ -179,112 +179,44 @@ public class DiagramData {
 
     @Getter
     @Setter
-    private static class RangeData implements QuestionData {
-        /* default */ long questionId;
-        /* default */ String questionTitle;
-        private List<String> answerPossibilities;
-        private List<Integer> data;
-        private List<Double> relative;
-        private String median;
-        private String mode;
+    protected static class RangeData extends ChoiceData implements QuestionData {
 
-        /* default */ RangeData(final long questionId, final String questionMessage ,
+        RangeData(final long questionId, final String questionMessage ,
                                 final double startValue, final double endValue, final double stepSize,
                                 final String lowerText, final String upperText) {
-            this.questionId = questionId;
-            this.questionTitle = questionMessage;
-            answerPossibilities = new ArrayList<>();
-            data = new ArrayList<>();
+            super(questionId, questionMessage, new ArrayList<>());
+            super.setData(new ArrayList<>());
             if(lowerText != null && !lowerText.isEmpty())
             {
-                answerPossibilities.add(lowerText);
-                data.add(0);
+                super.getAnswerPossibilities().add(lowerText);
+                super.getData().add(0);
             }
             for(double i = startValue; i < endValue;)
             {
-                answerPossibilities.add(i + " - " + (i += stepSize));
-                data.add(0);
+                super.getAnswerPossibilities().add(i + " - " + (i += stepSize));
+                super.getData().add(0);
             }
             if(upperText != null && !upperText.isEmpty())
             {
-                answerPossibilities.add(upperText);
-                data.add(0);
+                super.getAnswerPossibilities().add(upperText);
+                super.getData().add(0);
             }
         }
 
+        @Override
         public void addAnswer(final int answerPossibility) {
-            data.set(answerPossibility, data.get(answerPossibility) + 1);
+            super.getData().set(answerPossibility, super.getData().get(answerPossibility) + 1);
         }
 
-        @Override public QuestionType getQuestionType() {
+        @Override
+        public QuestionType getQuestionType() {
             return QuestionType.RANGE_QUESTION;
-        }
-
-        @Override public void statistics() {
-            int size = 0;
-            final List<Integer> maxima = new ArrayList<>();
-            int max = 0;
-            //Häufigste Antwort und Menge der Antworten raussuchen
-            for (int i = 0; i < data.size(); i++) {
-                size += data.get(i);
-                if (data.get(i) > max) {
-                    maxima.clear();
-                    maxima.add(i);
-                    max = data.get(i);
-                } else if (data.get(i) == max) {
-                    maxima.add(i);
-                }
-            }
-            final StringBuilder modeText = new StringBuilder();
-            for (final Integer i: maxima) {
-                modeText.append(answerPossibilities.get(i)).append(DIVIDER_STRING);
-            }
-            modeText.replace(modeText.lastIndexOf(DIVIDER_STRING), modeText.length(), "");
-            mode = modeText.toString();
-            //Median ist an mittlerer Stelle
-            int medianPos = size / 2;
-            relative = new ArrayList<>();
-            for (int i = 0; i < data.size(); i++) {
-                //Relative Häufigkeiten nur Summe vor Division
-                relative.add(data.get(i).doubleValue() / (double) size);
-                medianPos -= data.get(i);
-                // [1,1,1,1,1,1,2,2,2,3,3,3,4,6](originalDaten) => [0,6,3,3,1,0,1](data)
-                // => 14(data.size) => 7(/2) => 1(-6) => -2(-3) => 2(Position) => Median
-                if (medianPos <= 0 && median == null) {
-                    if (medianPos == 0 && i < answerPossibilities.size()) {
-                        median = answerPossibilities.get(i) + DIVIDER_STRING + answerPossibilities.get(i + 1);
-                    } else {
-                        median = answerPossibilities.get(i);
-                    }
-                }
-            }
-        }
-
-        public String toJSON() {
-            final StringBuilder json = new StringBuilder();
-            json.append("{\"id\": ").append(questionId)
-                    .append(",\"type\": ").append("\"choice\"")
-                    .append(",\"title\": \"").append(questionTitle)
-                    .append("\",\"answerPossibilities\": [");
-            for (int i = 0; i < answerPossibilities.size(); i++) {
-                json.append('\"').append(answerPossibilities.get(i)).append('\"');
-                if (i < answerPossibilities.size() - 1) {
-                    json.append(',');
-                }
-            }
-            json.append("],\"data\": ").append(data)
-                    .append(",\"calculated\": {")
-                    .append("\"relative\": ").append(relative)
-                    .append(",\"median\": \"").append(median)
-                    .append("\",\"mode\": \"").append(mode)
-                    .append("\"}}");
-            return json.toString();
         }
     }
 
     @Getter
     @Setter
-    private static class SliderData implements QuestionData {
+    protected static class SliderData implements QuestionData {
         /* default */ long questionId;
         /* default */ String questionTitle;
 
