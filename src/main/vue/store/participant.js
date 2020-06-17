49624fff0f9aed5e87022a9a5588919014778c25
nhille <nhille@techfak.uni-bielefeld.de>
2020-06-17 13:04:15 +0200

    #15522 @3.0h sortierfragen werden angezeigt und mit saveAnser zum store geschickt

--- src/main/vue/store/participant.js
+++ src/main/vue/store/participant.js
@@ -165,6 +165,8 @@ export const actions = {
      * @param answerObj object with (username, questionId, answerList, pollId)
      */
     saveAnswer(state, answerObj) {
+        console.log('answer in store')
+        console.log(answerObj)
         this.$axios.defaults.baseURL = 'http://localhost:8088/api/'
         this.$axios.defaults.headers.common.Authorization = 'Bearer ' + localStorage.getItem('user-token')
         return this.$axios.post('/poll/' + answerObj.pollId + '/addanswer', answerObj)
