2d1240078c506c3e8d5e2e2546d217e7c17e1e3a
Jeroshan Empeerious <jeroshan98@yahoo.de>
2020-06-22 12:53:41 +0200

    refs #15766 added a upload csv button in page @0.5h

--- src/main/vue/pages/MailSending.vue
+++ src/main/vue/pages/MailSending.vue
@@ -1,34 +1,50 @@
 <template>
     <div class="container">
         <div class="large-12 medium-12 small-12 cell">
-            <label>File
-                <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+            <label>
+                File
+                <input id="file" ref="file" type="file" @v-on:change="handleFileUpload()" />
             </label>
-            <button v-on:click="submitFile()">Submit</button>
+            <button @v-on:click="submitFile()">Submit</button>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "MailSending",
-        data() {
-            return {
-                file: '',
-            }
-        },
-        methods: {
-            handleFileUpload(){
-                this.file = this.$refs.file.files[0];
-            },
-            submitFile(){
-                let formData = new FormData();
-                formData.append('file', this.file);
-            }
+const axios = require('axios')
+
+export default {
+    data() {
+        return {
+            file: '',
         }
-    }
+    },
+
+    methods: {
+        submitFile() {
+            const formData = new FormData()
+
+            formData.append('file', this.file)
+
+            axios
+                .post('/single-file', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                })
+                .then(function () {
+                    console.log('SUCCESS!!')
+                })
+                .catch(function () {
+                    console.log('FAILURE!!')
+                })
+        },
+
+        handleFileUpload() {
+            this.file = this.$refs.file.files[0]
+        },
+    },
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
