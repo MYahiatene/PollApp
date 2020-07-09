8873c26dbcff5a5390d91e24b4672d036f5fa7cb
Luca Schlegel <admin@lucaschlegel.de>
2020-07-10 00:32:51 +0200

    Seite falls Poll bereits vorbei ist funktioniert

---
+++ src/main/vue/pages/pollOver.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <v-card class="pa-6">
+            <h1>Die Umfrage ist leider bereits vorbei.</h1>
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+    props: ['error'],
+    layout: 'blog',
+}
+</script>
