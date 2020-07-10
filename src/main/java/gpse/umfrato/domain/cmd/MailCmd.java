4cba2fd3b24d3b8d576b2bd1aa1cb5950ab3b0d7
Jeroshan Empeerious <jeroshan98@yahoo.de>
2020-07-10 09:04:48 +0200

    added the option to send remind mail to all/finished/not finished participants @1h

--- src/main/java/gpse/umfrato/domain/cmd/MailCmd.java
+++ src/main/java/gpse/umfrato/domain/cmd/MailCmd.java
@@ -9,6 +9,7 @@ public class MailCmd {
     private Long pollId;
     private String anonymityStatus;
     private List<String> csvFile;
+    private int notificateParticipants;
     private List<String> mailList;
     private String emailSubject;
     private String emailMessage;
