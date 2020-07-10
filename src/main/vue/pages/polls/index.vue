815cba23deabe2cc54c53a98fe98104f470d9acf
Jeroshan Empeerious <jeroshan98@yahoo.de>
2020-07-10 10:17:29 +0200

    select participants for mail sendings works @1h

--- src/main/vue/pages/polls/index.vue
+++ src/main/vue/pages/polls/index.vue
@@ -481,7 +481,7 @@ export default {
                 today.getMinutes()
             const poll = {
                 pollCreator: 'Jan',
-                anonymityStatus: '1',
+                anonymityStatus: '3',
                 pollName: 'Beispielumfrage',
                 activated: false,
                 deactivated: false,
@@ -493,8 +493,8 @@ export default {
                 visibility: true,
                 backgroundColor: '#555555',
                 fontColor: '#fe7312',
-                level: 0,
-                repeat: 1,
+                level: -1,
+                repeat: -1,
                 day: [],
                 week: [],
                 month: [],
